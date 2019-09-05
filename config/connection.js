ac21af436ca07a56f1f8c95db8f8fe3dfb6e11f2
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-09-04 17:37:38 -0700

    Added support for SQL on Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
 const mysql = require('mysql');
 
-const burgerDB = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:'H@rryP0tt3r',
-    port: 3306,
-    database: 'burgers_db'
-});
+let burgerDB; 
+
+
+if (process.env.JAWSDB_URL) {
+    burgerDB = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    burgerDB = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'H@rryP0tt3r',
+        port: 3306,
+        database: 'burgers_db'
+    });
+}
+
+
 
 module.exports = burgerDB;
